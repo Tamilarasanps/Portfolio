633f9ac8ec35c01946a32d24cdb324f117f7bda2
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-06-09 16:15:48 +0530

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./component/Header.jsx";
-import HomePage from "./component/HomePage.jsx";
-import About from "./component/About.jsx";
-import Skills from "./component/Skill.jsx";
-import Projects from "./component/Project.jsx";
-import Contact from "./component/Contact.jsx";
-import Footer from "./component/Footer.jsx";
-import Education from "./component/Education.jsx";
-import Services from "./component/Services.jsx";
-import AboutAndServices from "./component/About.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Header />
-    <HomePage />
-    <AboutAndServices />
-    <Skills />
-    <Projects />
-    <Education />
-    <Contact />
-    <Footer />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
